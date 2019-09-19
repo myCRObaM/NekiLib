d255d783081bc056ac36e6980f899ed8ce8580ae
myCRObaM <matej.hetzel@gmail.com>
2019-09-19 09:10:07 +0200

    NekiLib

--- Example/NekiLib/ViewController.swift
+++ Example/NekiLib/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import NekiLib
 
 class ViewController: UIViewController {
-
+    var customSwitch: CustomSwitch = {
+        let customSwitch = CustomSwitch()
+        customSwitch.translatesAutoresizingMaskIntoConstraints = false
+        customSwitch.onTintColor = UIColor.orange
+        customSwitch.offTintColor = UIColor.darkGray
+        customSwitch.cornerRadius = 0.1
+        customSwitch.thumbCornerRadius = 0.1
+        customSwitch.thumbTintColor = UIColor.white
+        customSwitch.animationDuration = 0.25
+        return customSwitch
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       setupUI()
     }
-
+    func setupUI(){
+        self.view.addSubview(customSwitch)
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            customSwitch.topAnchor.constraint(equalTo: self.view.centerYAnchor),
+            customSwitch.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            customSwitch.widthAnchor.constraint(equalToConstant: 80),
+            customSwitch.heightAnchor.constraint(equalToConstant: 30)])
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Example/Pods/Target Support Files/NekiLib/NekiLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NekiLib : NSObject
+@end
+@implementation PodsDummy_NekiLib
+@end
